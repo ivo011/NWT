1b1cd5327e575d19507c936b2c2e49c932c74254
ivo011 <ivo.split3@gmail.com>
2020-09-01 20:40:21 +0200

    post style

--- client/nwt-client/src/components/post.js
+++ client/nwt-client/src/components/post.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './post.css'
 
+
 const Post = () => {
     return ( 
         <div className="post">
             <div className="post-header">
-
+                <div className="user-details">
+                    <div className="details-picture"><img className="post-profile-pic" alt="" src=""/></div>
+                    <h3>Username</h3>
+                </div>
             </div>
             <div className="post-picture">
-
+                <img className="post-pic" alt="desc" src="/pic/pic1.jpg"/>
             </div>
             <div className="post-icons">
 
