db4c23ab7af8cdb11807e1a079955dc570241bd1
ivo011 <ivo.split3@gmail.com>
2020-09-12 21:27:30 +0200

    registerModal

---
+++ client/nwt-client/src/components/registerModal.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import './registerModal.css'
+import { Button, Input, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const RegisterModal = ({modal, toggle}) => {
+
+      
+    
+    const [username, setUsername] = useState(""); 
+    const [email, setEmail] = useState(""); 
+    const [password, setPassword] = useState(""); 
+    const [age, setAge] = useState(""); 
+
+    const [profilepic, setProfilepic] = useState("/pic/default.png");
+
+    const onChange = e =>{            
+        setProfilepic("/pic/" + e.target.files[0].name);        
+    }
+
+    return ( 
+        <div>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Register </ModalHeader>
+                    <ModalBody className="ModalBody">
+                        <Input type="username"  value={username} onChange={(e) => setUsername(e.target.value) } autoComplete="off" placeholder="Enter username" />
+                        <Input type="email"  value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off" placeholder="Enter email" />
+                        <Input type="password"  value={password} onChange={(e) => setPassword(e.target.value)}  placeholder="Enter password" />  
+                        <Input type="age" value={age} onChange={(e) => setAge(e.target.value)}  placeholder="Enter age" />    
+                        <div className="input-picture">
+                            <input type="file" name="inputfile" onChange={onChange}/>      
+                            <img className="profilepic" src={profilepic} alt=""/>  
+                        </div>                                                   
+                    </ModalBody>
+                <ModalFooter>
+                        <Button color="primary" onClick={toggle}>Confirm</Button>{' '}
+                        <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>        
+
+        </div>
+     );
+}
+ 
+export default RegisterModal;
--- client/nwt-client/src/pages/firstpage.js
+++ client/nwt-client/src/pages/firstpage.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../components/firstpage.css'
 import { Button, Input } from 'reactstrap';
+import RegisterModal from '../components/registerModal';
 
-const Firstpage = () => {
+const Firstpage = () => {    
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+   
     return (
          <div className="first-page">
-
                 <div className="content">
                     <h1>NWT Project</h1>
                     <h2>Full stack web application</h2>
-                    <h4>NodeJS | React | Postgres | Express | Javascript </h4>                    
+                    <h4>NodeJS | React | PostgreSQL | Express | Javascript </h4>                    
                 </div>
                
                 <div className="side">   
                     <div className="log-in">    
-                        <Input type="email" name="email" id="exampleEmail" placeholder="Enter email" />                                                    
+                        <Input type="email" name="email" autoComplete="off" id="exampleEmail" placeholder="Enter email" />                                                    
                         <Input type="password" name="password" id="examplePassword" placeholder="Enter password" />  
                         <Button className="login-button" color="primary">Log in</Button>               
                     </div>
                     <div className="register">                        
                         <h4>You don't have an account?</h4>
-                        <Button className="register-button" color="success">Register</Button>
-                    </div> 
-                 </div>           
-         </div> 
+                        <Button className="register-button" onClick={toggle} color="success">Register</Button>
+                    </div>                     
+                 </div>            
+                    <RegisterModal modal={modal} toggle={toggle}/> 
+             </div> 
          );
 }
  
