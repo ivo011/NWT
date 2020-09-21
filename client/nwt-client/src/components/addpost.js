9cb859447c0f121e52aaf3fa7fada790f30b5af7
ivo011 <ivo.split3@gmail.com>
2020-09-21 20:12:16 +0200

    uploading pictures in addpost

--- client/nwt-client/src/components/addpost.js
+++ client/nwt-client/src/components/addpost.js
@@ -27,7 +27,13 @@ const AddPost = () => {
          .then(res =>{                
             console.log(res);
          })         
-         .catch(err => console.log(err));                                  
+         .catch(err => console.log(err));   
+         setPostText("");  
+         setPostPicture(null);                             
+    }
+
+    const onChange = (e) =>{            
+        setPostPicture("/pic/" + e.target.files[0].name);        
     }
 
     return ( 
@@ -35,14 +41,18 @@ const AddPost = () => {
             <div className="addPostHeader">
                 <FormGroup className="addPostFormGroup">     
                     <img src={user.profilepic} alt=""/>               
-                    <Input type="textarea" name="text" id="exampleText" onChange = {e => setPostText(e.target.value)} />                    
+                    <Input type="textarea" name="text" value={postText} id="exampleText" onChange = {e => setPostText(e.target.value)} />                   
                 </FormGroup>
             </div>
             <div className="addPostIcons">
                 <div className="icons">
-                    <AiOutlinePicture size="2em"/>                    
-                    <AiOutlineSmile size="2em"/>
+                     <input type="file" id="file" onChange={onChange} />   
+                     <label htmlFor ="file">
+                        <AiOutlinePicture className="pictureIcon" size="2em"/>
+                    </label>              
+                    <AiOutlineSmile size="2em"/>                   
                 </div>
+                <img className="postImg" src={postPicture} alt=""/>  
                 <Button color="primary" onClick={handleAddPost}>Post</Button>
             </div>
 
